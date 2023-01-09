0fbd829a079d16bc7fb7012d165c1c509c86c0de
mgoester <goester@team23.de>
2023-01-09 15:39:30 +0100

    feat: add initial config

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const config = require('@commitlint/config-conventional');
+
+// add 'sec' type
+const types = config.rules['type-enum'][2].concat(['sec']);
+
+module.exports = {
+    // See https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-conventional/index.js
+    extends: ['@commitlint/config-conventional'],
+    // Own rules
+    rules: {
+        'subject-case': [
+            2,
+            'never',
+            ['start-case', 'pascal-case', 'upper-case'],
+        ],
+        'type-enum': [2, 'always', types],
+    },
+};
